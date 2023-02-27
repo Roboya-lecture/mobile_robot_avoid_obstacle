6c1e775905933f919878e3c3258fed5b261a7da3
seo2730 <42115807+seo2730@users.noreply.github.com>
2023-02-27 21:49:19 +0900

    Update setup.py

--- msg_tutorials/setup.py
+++ msg_tutorials/setup.py
@@ -2,17 +2,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args1 = generate_distutils_setup(
-    packages=['Pubmsg'],
+setup_args = generate_distutils_setup(
+    packages=['msg_tutorial'],
     package_dir={'': 'src'}
 )
 
-setup(**setup_args1)
-
-# fetch values from package.xml
-setup_args2 = generate_distutils_setup(
-    packages=['Submsg'],
-    package_dir={'': 'src'}
-)
-
-setup(**setup_args2)
+setup(**setup_args)
